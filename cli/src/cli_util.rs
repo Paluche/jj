abfa1cfc2b3b2c6840847d1ca024f76855a0535c
Hubert Lefevre <hubelefevre@gmail.com>
2025-12-20 15:29:28 +0100

    cli: Print newly tracked files
    XXX New idea:
    - Have the snapshot stats provide the list of newly tracked paths,
      untracked paths and ignored path as already collapsed.
    - Allows to collapse + sort properly. Then the display is much more
      simple.
    
    Tracking file automatically is a neat thing, until you did not want to
    track the file that just got auto-tracked. This could happen on many
    jj commands, so you could, easily end-up having unwanted files in your
    commits, without realizing you have them.
    
    We are now printing a message to indicate when jj starts auto-tracking
    files. With the intent to have a quick response from the user when one
    of the newly tracked files is not one they want to have tracked.
    
    The message to not take more one single line of your terminal, to not
    spam. It will list the path of as much newly tracked files as possible,
    and giving the actual number of files it could not print.
    
    Hopefully it should trigger the user into running another command
    (such as `jj status`) to check what is the state of the commit. And
    take appropriate action to ignore and untrack the unwanted files.

--- cli/src/cli_util.rs
+++ cli/src/cli_util.rs
@@ -433,8 +433,8 @@ impl CommandHelper {
     /// Loads workspace and repo, then snapshots the working copy if allowed.
     #[instrument(skip(self, ui))]
     pub fn workspace_helper(&self, ui: &Ui) -> Result<WorkspaceCommandHelper, CommandError> {
-        let (workspace_command, stats) = self.workspace_helper_with_stats(ui)?;
-        print_snapshot_stats(ui, &stats, workspace_command.env().path_converter())?;
+        let (workspace_command, _stats) = self.workspace_helper_with_stats(ui)?;
+        //print_snapshot_stats(ui, &stats, workspace_command.env().path_converter())?;
         Ok(workspace_command)
     }
 
@@ -1208,10 +1208,10 @@ impl WorkspaceCommandHelper {
     /// copy is collocated with Git.
     #[instrument(skip_all)]
     pub fn maybe_snapshot(&mut self, ui: &Ui) -> Result<(), CommandError> {
-        let stats = self
+        let _stats = self
             .maybe_snapshot_impl(ui)
             .map_err(|err| err.into_command_error())?;
-        print_snapshot_stats(ui, &stats, self.env().path_converter())?;
+        //print_snapshot_stats(ui, &stats, self.env().path_converter())?;
         Ok(())
     }
 
@@ -1983,10 +1983,13 @@ to the current parents may contain changes from multiple commits.
                 .commit("snapshot working copy")
                 .map_err(snapshot_command_error)?;
             self.user_repo = ReadonlyUserRepo::new(repo);
+
+            let new_tree = commit.tree();
         }
         locked_ws
             .finish(self.user_repo.repo.op_id().clone())
             .map_err(snapshot_command_error)?;
+
         Ok(stats)
     }
 
@@ -2889,11 +2892,86 @@ pub fn print_untracked_files(
     Ok(())
 }
 
+pub fn print_newly_tracked_files(
+    ui: &Ui,
+    stats: &SnapshotStats,
+    path_converter: &RepoPathUiConverter,
+    tree: &MergedTree,
+) -> io::Result<()> {
+    // TODO:
+    // - collapse the list of files when they belong to a same directory, see to
+    //   implement something similar to / or reuse
+    //   commands::status::visit_collapsed_untracked_files()
+    // - With the collapsed output, we can fall back to a message displayed
+    // which mimicks the status command output.
+    //  proposed format:
+    // ```
+    // Auto-tracking {nb_total} new files:
+    //     {file_path}
+    //     {dir_path} ({nb_files_in_dir)
+    //     ... and {remaining} other files.
+    // ```
+    // Last line appearing only when the message starts to be too long (~10
+    // lines maximum). Path and numbers of file printed with the diff-added
+    // color (green by default).
+    if stats.newly_tracked_paths.is_empty() {
+        return Ok(());
+    }
+    let Some(mut formatter) = ui.status_formatter() else {
+        return Ok(());
+    };
+
+    write!(formatter, "Auto-tracking ")?;
+    write!(
+        formatter.labeled("diff").labeled("added"),
+        "{}",
+        stats.newly_tracked_paths.len()
+    )?;
+    writeln!(
+        formatter,
+        " new file{}:",
+        if stats.newly_tracked_paths.len() > 1 {
+            "s"
+        } else {
+            ""
+        }
+    )?;
+
+    visit_collapsed_tracked_files(
+        stats.newly_tracked_paths,
+        stats.untracked_paths,
+        stats.ignored_paths,
+        tree,
+        |path, is_dir| {
+            write!(formatter.labeled("diff").labeled("added"), "A ");
+            if let Some(dir_files) = is_dir {
+                writeln!(
+                    formatter.labeled("diff").labeled("added"),
+                    "{} ({} files)",
+                    path.to_internal_dir_string(),
+                    dir_files
+                )?;
+            } else {
+                writeln!(
+                    formatter.labeled("diff").labeled("added"),
+                    "{}", path.as_internal_file_string());
+            }
+            Ok(())
+        },
+    )
+    .block_on()
+    .unwrap();
+
+    Ok(())
+}
+
 pub fn print_snapshot_stats(
     ui: &Ui,
     stats: &SnapshotStats,
     path_converter: &RepoPathUiConverter,
+    tree: &MergedTree,
 ) -> io::Result<()> {
+    print_newly_tracked_files(ui, stats, path_converter, tree)?;
     print_untracked_files(ui, &stats.untracked_paths, path_converter)?;
 
     let large_files_sizes = stats
@@ -4259,7 +4337,7 @@ fn warn_if_args_mismatch(
 pub async fn visit_collapsed_tracked_files(
     paths: &[&RepoPath],
     untracked_files: &[&RepoPath],
-    ignored_paths: &[&RepoPath],
+    ignored_paths: &[(&RepoPath, bool)],
     tree: &MergedTree,
     mut on_path: impl FnMut(&RepoPath, Option<usize>) -> Result<(), CommandError>,
 ) -> Result<(), CommandError> {
@@ -4290,7 +4368,8 @@ pub async fn visit_collapsed_tracked_files(
         parent: &RepoPath,
         paths: Vec<&RepoPath>,
         untracked_files: &[&RepoPath],
-        ignored_paths: &[&RepoPath],
+        ignored_files: &[&RepoPath],
+        ignored_dirs: &[&RepoPath],
         root_tree: &Merge<Tree>,
     ) -> Result<bool, CommandError> {
         Ok(if parent == RepoPath::root() {
@@ -4302,11 +4381,15 @@ pub async fn visit_collapsed_tracked_files(
         {
             // No dangling untracked files within.
             false
-        } else if ignored_paths
+        } else if ignored_files
             .iter()
             .any(|p| p.as_ref().starts_with(parent) || parent.starts_with(p))
         {
-            // No dangling ignored files and not within an ignored directory.
+            // No dangling ignored files within.
+            false
+        } else if ignored_dirs.iter().any(|p| parent.starts_with(p))
+        {
+            // Not within an ignored directory.
             false
         } else {
             // All tracked files at this path are listed to be printed.
@@ -4324,12 +4407,18 @@ pub async fn visit_collapsed_tracked_files(
         })
     }
 
+    let ignored_dirs = ignored_paths.iter().filter_map(|(p, is_dir)| is_dir.then_some(p)).collect();
+
+    let ignored_files = ignored_paths.iter().filter_map(
+        |(p, is_dir)| (!is_dir).then_some(p)).collect();
+
     while let Some((parent, paths)) = candidates.pop_first() {
         if is_collapsable(
             parent,
             paths.iter().map(|v| v.0).collect(),
             untracked_files,
-            ignored_paths,
+            ignored_files,
+            ignored_dirs,
             &root_tree,
         )
         .await?
@@ -4478,7 +4567,7 @@ mod tests {
     fn collect_collapsed_tracked_files_string(
         paths: &[&RepoPath],
         untracked_paths: &[&RepoPath],
-        ignored_paths: &[&RepoPath],
+        ignored_paths: &[(&RepoPath, bool)],
         tree: &MergedTree,
         prefix: &str,
     ) -> String {
@@ -4610,7 +4699,7 @@ mod tests {
         let ignored = files
             .into_iter()
             .filter_map(|(repo_path, file_state)| {
-                matches!(file_state, FileState::Ignored).then_some(*repo_path)
+                matches!(file_state, FileState::Ignored).then_some((*repo_path, false))
             })
             .collect_vec();
 
